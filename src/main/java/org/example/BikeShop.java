0a79306681867f9ff2ff5611bc1bc97c4cabcacd
PARICORP\DRudkova <deina.rudkovaschneider@pari.com>
2024-12-13 16:48:24 +0100

    endpoint created

--- src/main/java/org/example/BikeShop.java
+++ src/main/java/org/example/BikeShop.java
@@ -5,8 +5,14 @@ public class BikeShop {
     public static void main(String[] args) {
         port(8002);
 
-        get("/hello", (req, res) -> "Hello, Bike Shop");
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        });
 
-        get("/products", (req, res) -> "Hello. " + req.params(":name"));
+        get("/hello", (req, res) -> "A placeholder for a product");
+
+        get("/products/:name", (req, res) -> "Placement for " + req.params(":name"));
     }
 }
