4eaa1d888468d607871ea2c34b766ed1d0269f59
PARICORP\DRudkova <deina.rudkovaschneider@pari.com>
2025-01-23 16:25:22 +0100

    moved values from DatabaseConnection to resources and changed the code in the class DatabaseConnection

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -4,20 +4,29 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class DatabaseConnection {
     private static HikariDataSource dataSource;
 
-    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/online_bike_shop";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "tArakan123!";
-
     static {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(JDBC_URL);
-        config.setUsername(USERNAME);
-        config.setPassword(PASSWORD);
-        dataSource = new HikariDataSource(config);
+        Properties properties = new Properties();
+        try (InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream("database.properties")) {
+            if (input == null) {
+                System.out.println("unable to find database.properties");
+            } else {
+                properties.load(input);
+                HikariConfig config = new HikariConfig();
+                config.setJdbcUrl(properties.getProperty("jdbc.url"));
+                config.setUsername(properties.getProperty("jdbc.username"));
+                config.setPassword(properties.getProperty("jdbc.password"));
+                dataSource = new HikariDataSource(config);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public static DataSource getDataSource() {
